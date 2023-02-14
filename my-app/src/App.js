f5ebf3f4be71a926c17fdecbd9661c01cba397b8
sahida-akter <mssahidasmrity@gmail.com>
2023-02-14 19:31:08 +0600

    Upload Done

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+
+import List from "./Components/UniqueList/List"
+
+export default function App(){
+  return(
+    <>
+    <h1>
+      <List/>
+    </h1>
+    </>
+  )
+}
---
+++ my-app/src/Components/UniqueList/List.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const todos = [
+    {
+      id : uuidv4(), 
+      title : "todo1",
+      desc : "todo1 description 1",
+    },
+    { 
+      id : uuidv4(),   
+      title : "todo2",
+      desc : "todo1 description 2",
+    },
+    {
+        id : uuidv4(),   
+      title : "todo3",
+      desc : "todo1 description 3",
+    },
+    { 
+      id : uuidv4(),   
+      title : "todo4",
+      desc : "todo1 description 4",
+    }
+    
+    ];
+
+export default function List() {
+  return <div>
+        {todos.map((todo)=>{
+            const {id, title, desc} = todo
+            return <div key={id}>
+                <h3>{title}</h3>
+                <p>{desc}</p>
+            </div>
+        })}
+    </div>
+  
+};
